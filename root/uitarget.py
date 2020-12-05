799b117d651d5bd22ca825e2cf62e6f1ce47eb3a
ImperatorExterminatus <54247646+ImperatorExterminatus@users.noreply.github.com>
2020-12-05 22:09:17 +0100

    TARGET_INFO_EXTENDED FIX

--- root/uitarget.py
+++ root/uitarget.py
@@ -190,12 +190,12 @@ class TargetBoard(ui.ThinBoard):
 					nameLine.Show()
 					self.nameLine = nameLine
 
-					# if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
-					rarity = ui.TextLine()
-					rarity.SetParent(self)
-					rarity.SetPosition(32 + 5, 11)
-					rarity.Show()
-					self.rarity = rarity
+					if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
+						rarity = ui.TextLine()
+						rarity.SetParent(self)
+						rarity.SetPosition(32 + 5, 11)
+						rarity.Show()
+						self.rarity = rarity
 
 					self.SetSize(width, 32 + 5)
 
@@ -208,29 +208,29 @@ class TargetBoard(ui.ThinBoard):
 				def SetText(self, text):
 					self.nameLine.SetText(text)
 
-				# if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
-				def SetRarity(self, rarity):
-					if rarity <= 0:
-						return
+				if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
+					def SetRarity(self, rarity):
+						if rarity <= 0:
+							return
 
-					real_rarity = rarity / 10000
-					self.rarity.SetText(str(self.GetRarity(real_rarity)))
+						real_rarity = rarity / 10000
+						self.rarity.SetText(str(self.GetRarity(real_rarity)))
 
-				def GetRarity(self, rarity):
-					if rarity >= 100:
-						return "|cFFFFFFFFGuaranteed|r"
-					elif rarity < 100 and rarity >= 70:
-						return "|cFFFFFFFFCommon|r"
-					elif rarity < 70 and rarity >= 50:
-						return "|cFF32CD32Uncommon|r"
-					elif rarity < 50 and rarity >= 30:
-						return "|cFF9400D3Mythic|r"
-					elif rarity < 30 and rarity >= 11:
-						return "|cFF1E90FFRare|r"
-					elif rarity <= 10:
-						return "|cFFFFD700Legendary|r"
+					def GetRarity(self, rarity):
+						if rarity >= 100:
+							return "|cFFFFFFFFGuaranteed|r"
+						elif rarity < 100 and rarity >= 70:
+							return "|cFFFFFFFFCommon|r"
+						elif rarity < 70 and rarity >= 50:
+							return "|cFF32CD32Uncommon|r"
+						elif rarity < 50 and rarity >= 30:
+							return "|cFF9400D3Mythic|r"
+						elif rarity < 30 and rarity >= 11:
+							return "|cFF1E90FFRare|r"
+						elif rarity <= 10:
+							return "|cFFFFD700Legendary|r"
 							
-					return ""
+						return ""
 
 				def RefreshHeight(self):
 					ui.ListBoxExNew.Item.RefreshHeight(self)
@@ -426,11 +426,18 @@ class TargetBoard(ui.ThinBoard):
 						itemListBox = ui.ListBoxExNew(32 + 5, self.MAX_ITEM_COUNT)
 						itemListBox.SetSize(self.GetWidth() - 15 * 2 - ui.ScrollBar.SCROLLBAR_WIDTH, (32 + 5) * self.MAX_ITEM_COUNT)
 						height = 0
+						# for curItem in constInfo.MONSTER_INFO_DATA[race]["items"]:
+							# if curItem.has_key("vnum_list"):
+								# height += self.AppendItem(itemListBox, curItem["vnum_list"], curItem["count"])
+							# else:
+								# height += self.AppendItem(itemListBox, curItem["vnum"], curItem["count"])
+								
 						for curItem in constInfo.MONSTER_INFO_DATA[race]["items"]:
 							if curItem.has_key("vnum_list"):
-								height += self.AppendItem(itemListBox, curItem["vnum_list"], curItem["count"])
+								height += self.AppendItem(itemListBox, curItem["vnum_list"], curItem["count"], curItem["rarity"])
 							else:
-								height += self.AppendItem(itemListBox, curItem["vnum"], curItem["count"])
+								height += self.AppendItem(itemListBox, curItem["vnum"], curItem["count"], curItem["rarity"])
+								
 						if height < itemListBox.GetHeight():
 							itemListBox.SetSize(itemListBox.GetWidth(), height)
 						self.AppendWindow(itemListBox, 15)
@@ -492,8 +499,8 @@ class TargetBoard(ui.ThinBoard):
 				else:
 					myItem.SetText("%dx %s" % (count, itemName))
 					
-				# if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
-				myItem.SetRarity(rarity)
+				if app.ENABLE_SEND_TARGET_INFO_EXTENDED:
+					myItem.SetRarity(rarity)
 					
 				myItem.SAFE_SetOverInEvent(self.OnShowItemTooltip, vnum)
 				myItem.SAFE_SetOverOutEvent(self.OnHideItemTooltip)
